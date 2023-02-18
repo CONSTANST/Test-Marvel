5af3c0d12bd9394518d1ee335b452b73a58e4d2d
Thibault <thibault.constans@outlook.com>
2023-02-18 16:30:56 +0100

    commit

--- routes/comics.js
+++ routes/comics.js
@@ -28,7 +28,7 @@ router.get("/comics/:characterId", async (req, res) => {
     const characterId = req.params.characterId;
 
     const response = await axios.get(
-      `https://lereacteur-marvel-api.herokuapp.com/comics?apiKey=${apikey}&characterId=${characterId}`
+      `https://lereacteur-marvel-api.herokuapp.com/comics/${characterId}?apiKey=${apikey}`
     );
     // console.log(response.data);
     const comics = response.data;
